c8c7bbb0b06706b680db003887ccc2e7e0bb7567
majuncai <572000400@qq.com>
2017-08-11 10:37:21 +0800

    架构
    架构

---
+++ src/main/java/com/msproject/LarinApplication.java
@@ -0,0 +1,13 @@
+package com.msproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarinApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		SpringApplication.run(LarinApplication.class, args);
+	}
+}
